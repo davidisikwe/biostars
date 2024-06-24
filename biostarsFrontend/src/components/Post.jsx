d3cfe0aadbebbc0aade60a3ea34d151ed63226cf
David Isikwe <disikwe@gmail.com>
2024-06-24 00:48:48 -0500

    sunday commits

--- biostarsFrontend/src/components/Post.jsx
+++ biostarsFrontend/src/components/Post.jsx
@@ -39,13 +39,48 @@ const Views = ({ name, metric }) => {
   );
 };
 
-const Post = () => {
+const Tags = ({ tag }) => {
   return (
-    <div className='w-[]'>
+    <div
+      className='bg-[#ECECEC] text-[14px] text-[#6B3E4A] leading-none rounded-[2px] px-1 py-[1px] mr-[5px] inline-block border-b border-r border-[#bbbbbb] hover:bg-[#FBFDEF] hover:text-[#1E70BF]'
+      style={{ height: '22.8px', lineHeight: '20.8px' }}
+    >
+      {tag}
+    </div>
+  );
+};
+
+const Post = ({ postTitle, author, updatedBy }) => {
+  return (
+    <div className='w-[802.15px] py-[7px] border-b border-[#ececec] flex'>
       <div className='flex pr-[10px] pb-[8px] gap-[2px]'>
         <Vote name={'votes'} metric={3} />
         <Reply name={'reply'} metric={1} />
         <Views name={'views'} metric={30} />
+      </div>
+
+      <div className='w-full flex flex-col'>
+        <a className='text-[16px] text-[#4183C4] font-semibold'>{postTitle}</a>
+
+        <div className='flex justify-between mt-2 flex-wrap'>
+          <div className='flex flex-wrap gap-1'>
+            <Tags tag={'Bioinformatics'} />
+            <Tags tag={'BUSCO'} />
+            <Tags tag={'Digital twin'} />
+            <Tags tag={'Research'} />
+            <Tags tag={'TF'} />
+            <Tags tag={'Theory'} />
+            <Tags tag={'Theory'} />
+            <Tags tag={'Theory'} />
+            <Tags tag={'Theory'} /> <Tags tag={'Theory'} />
+          </div>
+          <span className='text-[14px] flex-shrink-0 w-full text-right'>
+            Updated 22 hours ago by{' '}
+            <a className='text-[#4183C4] text-[14px]'>{updatedBy}</a> • written
+            20 days ago by{' '}
+            <a className='text-[#4183C4] text-[14px]'>{author}</a> • 0
+          </span>
+        </div>
       </div>
     </div>
   );
--- biostarsFrontend/src/pages/Homepage.jsx
+++ biostarsFrontend/src/pages/Homepage.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Post from '../components/Post';
+import searchicon from '../assets/searchicon.svg';
+import { useState } from 'react';
 
 const SearchBar = () => {
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
-    <input
-      type='search'
-      placeholder='Search ...'
-      className='w-[442.5px] h-[37.6px] py-[9.5px] pl-[14px] pr-[37.4px] border bg-[#f8f8f9]'
-      name='Search Bar'
-    />
+    <div className='relative'>
+      <input
+        type='search'
+        placeholder='Search ...'
+        className='h-[37.6px] py-[9.5px] pl-[14px] pr-[40px] border bg-[#f8f8f9] rounded-[4px] focus:outline-none focus:ring focus:ring-blue-500 focus:ring-opacity-50'
+        style={{ width: '401.07px', borderWidth: '1px' }}
+        name='Search Bar'
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      />
+      <svg
+        className={`absolute top-1/2 right-3 transform -translate-y-1/2 w-5 h-5 pointer-events-none ${
+          isFocused ? 'text-blue-500' : 'text-gray-500'
+        }`}
+        xmlns='http://www.w3.org/2000/svg'
+        fill='none'
+        viewBox='0 0 24 24'
+        stroke='currentColor'
+      >
+        <path
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          strokeWidth='2'
+          d='M10 6a4 4 0 100 8 4 4 0 000-8zM21 21l-4.35-4.35'
+        />
+      </svg>
+    </div>
   );
 };
 
@@ -19,7 +44,17 @@ const Homepage = () => {
       <Navbar />
       <SearchBar />
       <main className='mt-10'>
-        <Post />
+        <Post
+          postTitle={'Phlogeny away alignment'}
+          author={'tchosange'}
+          updatedBy={'disikwe'}
+        />
+        <Post
+          postTitle={'2nd Post'}
+          author={'tchosange'}
+          updatedBy={'disikwe'}
+        />
+        <Post postTitle={'prop'} author={'tchosange'} updatedBy={'habeeb'} />
       </main>
     </div>
   );
